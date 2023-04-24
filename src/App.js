8a9064c46a7ed9037b5a94a539dd054a1faac01d
Noora <noora.toimela@gmail.com>
2023-04-24 16:26:33 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Journeys } from '';
+import { StationsList } from '';
+import { StationDetails } from '';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <Header />
+        <h1>Helsinki City Bike App</h1>
       </header>
-    </div>
+      {/* <Switch>
+        <Route path="">
+          <Journeys />
+        </Route>
+        <Route path="">
+          <StationsList />
+        </Route>
+        <Route path="">
+          <StationDetails />
+        </Route>
+      </Switch> */}
+    </Router>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <h1>Helsinki City Bike App</h1>
+    );
+}
