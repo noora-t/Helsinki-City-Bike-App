073104b2d1edc439621a013a0ba69d7f9e321db9
Noora <noora.toimela@gmail.com>
2023-04-24 16:50:53 +0300

    Create Journeys page

--- src/App.js
+++ src/App.js
@@ -2,28 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Header } from './components/Header';
-import { Journeys } from '';
-import { StationsList } from '';
-import { StationDetails } from '';
+import { Journeys } from './components/Journeys';
+// import { StationsList } from '';
+// import { StationDetails } from '';
 
 function App() {
   return (
     <Router>
       <header>
         <Header />
-        <h1>Helsinki City Bike App</h1>
       </header>
-      {/* <Switch>
+      <Switch>
         <Route path="">
           <Journeys />
         </Route>
-        <Route path="">
+        {/* <Route path="">
           <StationsList />
         </Route>
         <Route path="">
           <StationDetails />
-        </Route>
-      </Switch> */}
+        </Route> */}
+      </Switch>
     </Router>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
     return (
-        <h1>Helsinki City Bike App</h1>
+        <div>
+            <h1>Helsinki City Bike App</h1>
+        </div>
     );
 }
+
